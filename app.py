c2faf8a731c412b55309f37b04fe4dd006b5c950
Morgan-Elkins <me839@york.ac.uk>
2025-01-29 15:02:51 +0000

    Sending email works

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import json
 import os
 import time
 
+from botocore.exceptions import ClientError
 from dotenv import load_dotenv
 import boto3
 from flask import Flask
@@ -10,7 +11,77 @@ load_dotenv()
 
 AWS_REGION = os.getenv("AWS_REGION")
 AWS_QUEUE = os.getenv("AWS_Q3")
+SENDER_EMAIL = os.getenv("SENDER_EMAIL")
+RECIPIENT_EMAIL = os.getenv("RECIPIENT_EMAIL")
+
 sqs = boto3.client('sqs', region_name=AWS_REGION)
+client = boto3.client('ses',region_name=AWS_REGION)
+
+CONFIGURATION_SET = "ConfigSet"
+
+
+def send_email():
+    # The subject line for the email.
+    SUBJECT = "Amazon SES Test (SDK for Python)"
+
+    # The email body for recipients with non-HTML email clients.
+    BODY_TEXT = ("Amazon SES Test (Python)\r\n"
+                 "This email was sent with Amazon SES using the "
+                 "AWS SDK for Python (Boto)."
+                 )
+
+    # The HTML body of the email.
+    BODY_HTML = """<html>
+    <head></head>
+    <body>
+      <h1>Amazon SES Test (SDK for Python)</h1>
+      <p>This email was sent with
+        <a href='https://aws.amazon.com/ses/'>Amazon SES</a> using the
+        <a href='https://aws.amazon.com/sdk-for-python/'>
+          AWS SDK for Python (Boto)</a>.</p>
+    </body>
+    </html>
+                """
+
+    # The character encoding for the email.
+    CHARSET = "UTF-8"
+
+    # Try to send the email.
+    try:
+        # Provide the contents of the email.
+        response = client.send_email(
+            Destination={
+                'ToAddresses': [
+                    RECIPIENT_EMAIL,
+                ],
+            },
+            Message={
+                'Body': {
+                    'Html': {
+                        'Charset': CHARSET,
+                        'Data': BODY_HTML,
+                    },
+                    'Text': {
+                        'Charset': CHARSET,
+                        'Data': BODY_TEXT,
+                    },
+                },
+                'Subject': {
+                    'Charset': CHARSET,
+                    'Data': SUBJECT,
+                },
+            },
+            Source=SENDER_EMAIL,
+            # If you are not using a configuration set, comment or delete the
+            # following line
+            # ConfigurationSetName=CONFIGURATION_SET,
+        )
+    # Display an error if something goes wrong.
+    except ClientError as e:
+        print(e.response['Error']['Message'])
+    else:
+        print("Email sent! Message ID:"),
+        print(response['MessageId'])
 
 def create_app():
     app = Flask(__name__)
@@ -64,4 +135,5 @@ def get_messages():
 if __name__ == '__main__':
     # app = create_app()
     # app.run()
-    get_messages()
+    # get_messages()
+    send_email()
